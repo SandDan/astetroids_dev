bc810f4f6fed8b0c71f53c7e876b9ead257a01a4
Daniel <sanddan117@yahoo.com>
2024-10-15 13:15:04 -0500

    Added player movement. Grouped player updates and drawables

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
 import pygame
 
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    #Initialize groups first
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    #Create player and add to groups
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    updatable.add(player)
+    drawable.add(player)
+    
+    #Set the static containers
+    Player.containers = (updatable, drawable)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill(pygame.Color("BLACK"))
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+        
     
-
+ 
 if __name__ == "__main__":
     main()
